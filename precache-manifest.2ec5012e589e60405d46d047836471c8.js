f347acbd67f2f68fde78c5447499ecf8c20da09b
duc.bang <nguyenducbang2018@gmail.com>
2020-08-04 09:49:41 +0700

    Updates

---
+++ precache-manifest.2ec5012e589e60405d46d047836471c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa2a2750662bdf6f9c85ee09744f09f",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "a493a9192ce684a3c6d1",
+    "url": "/netflix-clone/static/css/main.a54da055.chunk.css"
+  },
+  {
+    "revision": "99359bc766159d956517",
+    "url": "/netflix-clone/static/js/2.40b69551.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.40b69551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a493a9192ce684a3c6d1",
+    "url": "/netflix-clone/static/js/main.313d429f.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
